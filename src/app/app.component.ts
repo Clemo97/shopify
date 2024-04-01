51d17214eb46a5ed6385f95fe58ecedebbc21fd0
Clemo97 <lumumbaclement@gmail.com>
2024-04-01 19:03:54 +0300

    shopify variant form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shopify';
+// app.component.ts
+export class AppComponent implements OnInit {
+  productForm: FormGroup;
+  submittedData: any[] = [];
+  options: Option[] = [
+    { value: 'color', viewValue: 'Color' },
+    { value: 'size', viewValue: 'Size' },
+    { value: 'material', viewValue: 'Material' },
+    { value: 'style', viewValue: 'Style' }
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      name: 'size',
+      quantities: this.fb.array([
+        this.newQuantity()
+      ])
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  quantities(): FormArray {
+    return this.productForm.get("quantities") as FormArray
+  }
+
+  newQuantity(): FormGroup {
+    return this.fb.group({
+      qty: '',
+    });
+  }
+
+  addQuantity() {
+    this.quantities().push(this.newQuantity());
+  }
+
+  removeQuantity(i: number) {
+    this.quantities().removeAt(i);
+  }
+
+  onInput(index: number) {
+    const quantitiesArray = this.quantities();
+    if (quantitiesArray && quantitiesArray.length > index) {
+      const currentQuantityControl = quantitiesArray.at(index);
+      if (currentQuantityControl) {
+        const qtyControl = currentQuantityControl.get('qty');
+        if (qtyControl) {
+          const currentQuantity = qtyControl.value;
+          const isLastField = index === quantitiesArray.length - 1;
+          if (currentQuantity && isLastField) {
+            this.addQuantity();
+          }
+        }
+      }
+    }
+  }
+
+
+
+  onSubmit() {
+    console.log(this.productForm.value);
+    this.submittedData.push(this.productForm.value);
+    this.productForm.reset();
+  }
+
 }
+
+export interface Option {
+  value: string;
+  viewValue: string;
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
